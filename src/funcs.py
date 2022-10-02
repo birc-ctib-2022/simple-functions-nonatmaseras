644cff135758e5810e1cb4f00e1cb7e0f459dee1
Genona Torruella <genona.torruella@gmail.com>
2022-10-02 17:21:41 +0200

    test_funcs

--- src/funcs.py
+++ src/funcs.py
@@ -1,24 +1,25 @@
 """Exercises with simple functions"""
-
+from math import sqrt
 
 def prod(a, b, c):
     """
     Compute the product of three numbers.
 
     >>> prod(1, 2, 3)
-    'TEST ME!'
+    3
     """
-    ...
+    return a*b*c
 
-
+a = 2
 def prod2(b):
     """
     Get a global a and write to a local c before computing prod(a, b, c)
 
-    >>> prod2(42)
-    'TEST ME'
+    >>> prod2(4)
+    24
     """
-    ...
+    c = 3
+    return prod(ab.c)
 
 
 def longest(x, y):
@@ -36,8 +37,8 @@ def dist(p1, p2):
     Compute the distance between p1 and p2.
 
     >>> dist((1,2), (3,4))
-    'TEST ME'
+    sqrt(8)
     """
     x1, y1 = p1
     x2, y2 = p2
-    ...
+    return sqrt((x1-x2)**2 + (y1-y2)**2)
